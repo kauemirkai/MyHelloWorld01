ddfbf5833cf750730e49a8c6d40c2e35c5e60162
kauemirkai <kaue.mirkai@gmail.com>
2018-04-01 12:48:23 -0300

    first commit

---
+++ app/src/main/java/devmob/ftce/usjt/br/meuolamundo/MainActivity.java
@@ -0,0 +1,20 @@
+package devmob.ftce.usjt.br.meuolamundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+@author:Kaue mirkai
+RA:81613004
+Professor:Bonato
+Turma:CCP3AN-MCA
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
